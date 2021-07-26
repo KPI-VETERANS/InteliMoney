1adad4acb1237511dd48df04397fe2d4eac436fe
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2021-07-26 13:07:43 +0300

    First commit

---
+++ InteliMoney/InteliMoney/InteliMoney/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using InteliMoney.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace InteliMoney.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
